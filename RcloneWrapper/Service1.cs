e960237b39bef96a2ef3565a4e43fdd362652826
John Gakhokidze <66442890+virtiogroove@users.noreply.github.com>
2020-09-01 13:36:53 -0500

    Update Service1.cs

--- RcloneWrapper/Service1.cs
+++ RcloneWrapper/Service1.cs
@@ -21,12 +21,12 @@ namespace RcloneWrapper
         }
         protected override void OnStart(string[] args)
         {
-            WriteToFile("Service is started at " + DateTime.Now);
+            WriteToFile("Service started " + DateTime.Now);
             timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
-            timer.Interval = 5000; //number in milisecinds  
+            timer.Interval = 20000; 
             timer.Enabled = true;
 
-            var cPath = "C:\\rclone\\";
+          
             var cParams = "mount blob:poc c:\\Azure";
             string filename = "rclone.exe";
             try
@@ -36,39 +36,13 @@ namespace RcloneWrapper
             catch {
                 WriteToFile("Error");
             }
-            //WriteToFile(proc.ExitCode.ToString());
-
-            /*const string ex1 = "mount";
-            const string ex2 = "blob:poc";
-            const string ex3 = "c:\\Azure";
-
-            // Use ProcessStartInfo class
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = false;
-            startInfo.UseShellExecute = false;
-            startInfo.FileName = "rclone.exe";
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.Arguments = ex1 + " " + ex2 +" " +ex3;
-
-            try
-            {
-                // Start the process with the info we specified.
-                // Call WaitForExit and then the using statement will close.
-                using (Process exeProcess = Process.Start(startInfo))
-                {
-                    exeProcess.WaitForExit();
-                }
-            }
-            catch
-            {
-                // Log error.
-            }*/
+            
         }
         protected override void OnStop()
         {
 
-            WriteToFile("Service is stopped at " + DateTime.Now);
-           // var cPath = "C:\\rclone\\";
+            WriteToFile("Service stopped " + DateTime.Now);
+       
             var cParams = "/F /IM rclone.exe";
             string filename = "taskkill.exe";
             try
@@ -79,35 +53,11 @@ namespace RcloneWrapper
             {
                 WriteToFile("Error");
             }
-            // Use ProcessStartInfo class
-            /* ProcessStartInfo startInfo = new ProcessStartInfo();
-             startInfo.CreateNoWindow = false;
-             startInfo.UseShellExecute = true;
-             startInfo.FileName = "rclone.exe";
-             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-             startInfo.Arguments = ex1 + " " + ex2 + " " + ex3;
-
-             try
-             {
-                 // Start the process with the info we specified.
-                 // Call WaitForExit and then the using statement will close.
-                 using (Process exeProcess = Process.Start(startInfo))
-                 {
-                     WriteToFile(startInfo.ErrorDialog.ToString() + DateTime.Now);
-                     exeProcess.WaitForExit();
-
-
-
-                 }
-             }
-             catch
-             {
-                 // Log error.
-             }*/
+          
         }
         private void OnElapsedTime(object source, ElapsedEventArgs e)
         {
-            WriteToFile("Service is recall at " + DateTime.Now);
+            WriteToFile("Service is still alive " + DateTime.Now);
         }
         public void WriteToFile(string Message)
         {
@@ -116,7 +66,7 @@ namespace RcloneWrapper
             {
                 Directory.CreateDirectory(path);
             }
-            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\RcloneWrapper_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
             if (!File.Exists(filepath))
             {
                 // Create a file to write to.   
